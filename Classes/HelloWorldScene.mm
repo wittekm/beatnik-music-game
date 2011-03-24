277ed1275e530d760ac22449b0ad600b236d43e7
BarrelRolled@gmail.com <BarrelRolled@gmail.com@7310708a-dcbb-6125-ca74-c74a6c3ceb28>
2011-03-24 00:35:40 +0000

    added menuscene file

--- Classes/HelloWorldScene.mm
+++ Classes/HelloWorldScene.mm
@@ -56,44 +56,6 @@ HitObjectDisplay* HODFactory(HitObject* hitObject, int r, int g, int b) {
 }
 
 // HelloWorld implementation
-@implementation Layer1
-+(id) scene
-{
-	// 'scene' is an autorelease object.
-	CCScene *scene = [CCScene node];
-	
-	// 'layer' is an autorelease object.
-	Layer2 *layer = [Layer1 node];
-	
-	// add layer as a child to scene
-	[scene addChild: layer];
-	
-	// return the scene
-	return scene;
-}
-
--(id) init
-{
-	if( (self=[super init])) {
-
-	[CCMenuItemFont setFontSize:30];
-	[CCMenuItemFont setFontName: @"Courier New"];
-	
-	CCMenuItem *item1 = [CCMenuItemFont itemFromString: @"Press me to start!!" target: self selector:@selector(menuCallbackStart:)];
-	CCMenu *menu = [CCMenu menuWithItems:
-					item1, nil];
-	[self addChild: menu];
-		
-	}
-	return self;
-}
--(void) menuCallbackStart: (id) sender
-{
-	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:0.5f scene:[Layer2 scene]]];
-	//[(CCMultiplexLayer*)parent_ switchTo:2];
-}
-
-@end
 
 @implementation Layer2
 
