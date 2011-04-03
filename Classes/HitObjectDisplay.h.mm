bcbd428eb7f1458a9fa41463e2536b2dd02dc57c
stultissimus@gmail.com <stultissimus@gmail.com@7310708a-dcbb-6125-ca74-c74a6c3ceb28>
2011-04-03 21:05:08 +0000

    started migrating timing of disappearance over to hitobjectdisplay side

--- Classes/HitObjectDisplay.h.mm
+++ Classes/HitObjectDisplay.h.mm
@@ -10,13 +10,14 @@
 #import "cocos2d.h"
 #import "osu-import.h.mm"
 
+@class GameScene; // forward decl???
+
 @interface HitObjectDisplay : CCLayer {
 	HitObject * hitObject;
 	int red;
 	int green;
 	int blue;
 	double initialScale;
-	
 }
 
 - (id) initWithHitObject: (HitObject*)hitObject_ red: (int)r green: (int)g blue: (int)b;
@@ -24,8 +25,13 @@
 - (void) appearWithDuration: (double)duration;
 - (void) setOpacity: (GLubyte) opacity;
 
+
+- (int) disappearTime;
 - (BOOL) wasHit: (CGPoint)location atTime: (NSTimeInterval)time;
 - (BOOL) wasHeld: (CGPoint)location atTime: (NSTimeInterval)time;
+
+// shortcut to cast parent as a GameScene
+- (GameScene*) gsParent;
 
 // stuff pertaining to touch processing
 
